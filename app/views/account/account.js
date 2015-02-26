edcb1cdfde6eee48a440b7c28fe1df33129ff04f
Jamie Czerwinski <jamie.czerwinski@gmail.com>
2015-02-26 12:38:15 -0700

    fix(account): Fixes script error that prevented a user's Account page from displaying properly.
    Closes #30

--- app/views/account/account.js
+++ app/views/account/account.js
@@ -1,16 +1,10 @@
 angular.module('ethiveApp').controller('AccountCtrl', function($rootScope, $scope, $modal) {
     $rootScope.setTitle('Your account' + $rootScope.titleEnd);
     if ($rootScope.user) {
-        $scope.open = function(size) {
+        $scope.open = function() {
             var modalInstance = $modal.open({
                 templateUrl: '/views/provider/new/new.html',
                 controller: 'NewProviderCtrl'
-                //size: size,
-                /*resolve: {
-                        items: function() {
-                            return $scope.items;
-                        }
-                    }*/
             });
         };
         $rootScope.user.$refresh();
