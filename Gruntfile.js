bf1042b6dd7a7f6f3c9f943e357eb10cf586c94d
Jamie Czerwinski <jamie.czerwinski@gmail.com>
2015-05-24 13:27:45 -0600

    fix(images): wrong location. add favicons to build script

--- Gruntfile.js
+++ Gruntfile.js
@@ -303,9 +303,9 @@ module.exports = function (grunt) {
 					dest: '<%= yeoman.dist %>',
 					src: [
 						'*.{ico,png,txt}',
-						'.htaccess',
 						'*.html',
-						'images/{,*/}*.{webp}',
+						'favicons/*',
+						'images/*',
 						'fonts/*',
 						'divshot.json'
 					]
