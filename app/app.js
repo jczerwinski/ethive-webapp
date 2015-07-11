7b8ee9a46bc740d0b57edf4b21cf3e4bf3aee209
Jamie Czerwinski <jamie.czerwinski@gmail.com>
2015-07-11 13:57:18 -0600

    feat(global hotkeys): add home, account hotkeys

--- app/app.js
+++ app/app.js
@@ -11,11 +11,14 @@ import errors from 'components/errors/errors';
 import routes from 'routes/routes';
 import header from 'components/header/header';
 
+import 'chieffancypants/angular-hotkeys';
+
 export default angular.module('ethiveApp', [
 		'ui.router',
 		'restmod',
 		'angulartics',
 		'angulartics.google.analytics',
+		'cfp.hotkeys',
 		baseModel.name,
 		routes.name,
 		header.name,
@@ -26,9 +29,26 @@ export default angular.module('ethiveApp', [
 
 		$locationProvider.html5Mode(true);
 	}])
-	.run(['$rootScope', '$timeout', function ($rootScope, $timeout) {
+	.run(['$rootScope', '$timeout', 'hotkeys', '$state', function ($rootScope, $timeout, hotkeys, $state) {
 		// Expose lodash
 		$rootScope._ = _;
+
+		// Bind global hotkeys
+		hotkeys.bindTo($rootScope).add({
+			combo: 'a',
+			description: 'Cancel create new provider',
+			callback: function (event) {
+				event.preventDefault();
+				$state.go('account');
+			}
+		}).add({
+			combo: 'h',
+			description: 'Go to home page',
+			callback: function (event) {
+				event.preventDefault();
+				$state.go('home');
+			}
+		});
 
 		// Using setTitle() preserves history when changing title from controllers in ui-router states. By default, prepends a sitewide ending to the title. Can be disabled by setting omitEnd to true.
 		var defaultTitle = 'ethive';
