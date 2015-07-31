8bbb077f2214bc51dec2167b7abfc4d7d08f59f8
Jamie Czerwinski <jamie.czerwinski@gmail.com>
2015-07-31 12:47:15 -0600

    fix(selectOnFocus): fix issue where selected text would be immediately deselected on focus

--- app/components/selectOnFocus/selectOnFocus.js
+++ app/components/selectOnFocus/selectOnFocus.js
@@ -1,12 +1,15 @@
 import angular from 'angular';
 
-export default angular.module('ethiveSelectOnFocus', []).directive('selectOnFocus', [function () {
-    return {
-        restrict: 'A',
-        link: function (scope, element, attrs) {
-            element.on('focus', function () {
-                this.select();
-            });
-        }
-    };
+export default angular.module('ethiveSelectOnFocus', []).directive('selectOnFocus', ['$timeout', function ($timeout) {
+	return {
+		restrict: 'A',
+		link: function (scope, element, attrs) {
+			element.on('focus', function () {
+				var self = this;
+				$timeout(function () {
+					self.select();
+				})
+			});
+		}
+	};
 }]);
